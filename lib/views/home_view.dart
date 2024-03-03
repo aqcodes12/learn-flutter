316f4762aec21556feb535923cbd543dc787d32f
Abdul Qayyum <aqcodes12@gmail.com>
2024-03-03 17:22:59 +0530

    first commit

---
+++ lib/views/home_view.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_learn/views/calculator_view.dart';
+
+class HomeView extends StatelessWidget {
+  const HomeView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      backgroundColor: Colors.amberAccent,
+      body: SafeArea(
+        bottom: false,
+        child: CalculatorView(),
+      ),
+    );
+  }
+}
